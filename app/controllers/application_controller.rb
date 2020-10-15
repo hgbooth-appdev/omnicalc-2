7d7dfb3e9caae5c809f56843c030c86d2cd49eca
hgbooth <hgregor0@chicagobooth.edu>
2020-10-15 21:11:36 +0000

    add and subtract

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
 
+
+    # ====================== Addition =====================
+    def additionForm
+      render({ :template => "/First4/addForm.html.erb"})
+
+    end
+    
+    def additionRes
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @res = @first + @second
+
+      render({ :template => "/First4/addRes.html.erb"})
+    end
+
+    # ====================== Subtraction =====================
+    def subtractionForm
+      render({ :template => "/First4/subtractForm.html.erb"})
+
+    end
+    
+    def subtractionRes
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @res = @second - @first
+
+      render({ :template => "/First4/subtractRes.html.erb"})
+    end
+
+    # ====================== Multiplication =====================
+
+
+    # ====================== Division =====================
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", {:controller => "application", :action => "additionForm"})
+  get("/add", {:controller => "application", :action => "additionForm"})
+  get("/wizard_add", {:controller => "application", :action => "additionRes"})
+  
+  get("/subtract", {:controller => "application", :action => "subtractionForm"})
+  get("/wizard_subtract", {:controller => "application", :action => "subtractionRes"})
+
 end
